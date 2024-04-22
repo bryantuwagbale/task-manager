15db2f8a4b1d681fb5529af5af2f12069dcf7e86
Bryant Uwagbale <bryantu1997@gmail.com>
2024-04-22 18:08:58 -0500

    14:04 Global Styles Provider

---
+++ app/providers/GlobalStyleProvider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from 'react'
+
+interface Props{
+    children: React.ReactNode;
+}
+
+function GlobalStyleProvider({ children }: Props) {
+    return <>{children}</>;
+}
+export default GlobalStyleProvider
